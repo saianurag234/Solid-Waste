2c40b43b2f42f64ed83eeeb7dc726553bf02630e
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-03-19 19:18:34 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -9,23 +9,3 @@ st.image(img_url, width=650)
 want_to_contribute = st.button("Click Here to upload the Image")
 if want_to_contribute:
     switch_page("Upload Image")
-
-# Use the Streamlit `beta_container()` function to create a container
-button_container = st.beta_container()
-
-# Add the left button to the container using `button_container.button()`
-with button_container:
-    left_button = st.button("Left button")
-
-# Add the right button to the container using `button_container.button()`
-with button_container:
-    right_button = st.button("Right button", key="right_button", help="This is the right button")
-
-# Use CSS to position the buttons within the container
-button_container.write("""
-<style>
-div.stButton > button:first-child {
-    margin-right: 1em;
-}
-</style>
-""", unsafe_allow_html=True)

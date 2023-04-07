88a12f7377ff079794873d2af30b7978dcf46d2b
saianurag234 <108020903+saianurag234@users.noreply.github.com>
2023-04-07 21:22:53 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -3,17 +3,6 @@ from streamlit_extras.switch_page_button import switch_page
 
 st.title("Solid Waste Classification")
 
-st.markdown(
-   f”””
-   <style>
-   p {
-   background-image: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fvector%2Frubbish-bins-for-recycling-different-types-of-waste-on-city-background-sort-plastic-gm1182399503-332004011&psig=AOvVaw2rgWfz9fRBHoqgK_htHsvH&ust=1680871834381000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKCX79allf4CFQAAAAAdAAAAABAE');
-   }
-   </style>
-   ”””,
-   unsafe_allow_html=True)
-
-
 
 want_to_contribute = st.button("Click Here to upload the Image")
 if want_to_contribute:
